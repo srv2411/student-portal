db5d3e34fd0f9272f38adde7f2b8a936b68e5bc7
sourav <srvsahusrv@gmail.com>
2026-08-16 19:18:50 +0530

    add main.cpp with greeting logic

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+std::string greetUser(const std::string& name) {
+    return "Welcome to Student Portal, " + name + "!";
+}
+
+int main() {
+    std::string name = "Student";
+    std::cout << greetUser(name) << std::endl;
+    return 0;
+}
