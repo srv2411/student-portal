2d3138f5b9e10867d2d5ba6e3399170c51c9973c
sourav <srvsahusrv@gmail.com>
2026-08-16 23:23:08 +0530

    Update greeting directly on main

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ std::string greetUser(const std::string& name) {
 
 int main() {
     std::string name = "Student";
-    std::cout << greetUser(name) << std::endl;
+    std::cout <<"Main Version : "<< greetUser(name) << std::endl;
     return 0;
 }
 // important change
