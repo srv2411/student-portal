31ca3ed2f4ee01dd7ac91a083a7a4bff184de647
sourav <srvsahusrv@gmail.com>
2026-08-16 23:22:05 +0530

    Update greeting on conflict-demo branch

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ std::string greetUser(const std::string& name) {
 
 int main() {
     std::string name = "Student";
-    std::cout << greetUser(name) << std::endl;
+    std::cout << "Branch Version : "<< greetUser(name) << std::endl;
     return 0;
 }
 // important change
